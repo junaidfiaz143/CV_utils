1f7967b758581115d1dfdfb455f1baefe425668a
M.Junaid Fiaz <junaidfiaz143@gmail.com>
2019-09-19 23:24:14 +0500

    update create_masks.py with details

--- create_mask.py
+++ create_mask.py
@@ -1,15 +1,18 @@
+# CREDIT: https://github.com/savankoradiya/Extract-Mask-From-COCO-Dataset/blob/master/extract_mask_from_coco.py
 from pycocotools.coco import COCO
 import numpy as np
 import cv2
 import os
-#Add your coco dataset images folder path
+
 data_dir = 'val2017'
 ann_file='instances_val2017.json'
-#Add your output mask folder path
+
 seg_output_path = 'dataset/coco/seg'
-#Store original images into another folder
+
 original_img_path = 'dataset/coco/img'
+
 coco = COCO(ann_file)
+
 catIds = coco.getCatIds(catNms=['person']) #Add more categories ['person','dog']
 imgIds = coco.getImgIds(catIds=catIds )
 
@@ -18,12 +21,19 @@ for i in range(len(imgIds)):
 	annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=0)
 	anns = coco.loadAnns(annIds)
 	mask = coco.annToMask(anns[0])
-	for i in range(len(anns)):
+
+	for i in range(len(anns)): #get if number annotations contains number of masks
 		mask += coco.annToMask(anns[i])
+
+	# print(mask.shape)
 	file_name = os.path.join(data_dir,img['file_name'])
 	original_img = cv2.imread(file_name)
 	cv2.imwrite(os.path.join(original_img_path,img['file_name']),original_img)
 	cv2.imwrite(os.path.join(seg_output_path,img['file_name']),mask*255)
+
+	# cv2.imshow(os.path.join(seg_output_path,img['file_name']),mask*255)
+	# cv2.waitKey(0)
+	
 	# print(mask)
 	# print(mask*255)
 	print("processing...")
